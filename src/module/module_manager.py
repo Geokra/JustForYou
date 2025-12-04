b5a071f19cd4bd01adaf87ea7ab13c22fc7b42b8
1catan <jonaskipper0@gmail.com>
2025-12-04 20:27:01 +0100

    feat: implement module system
    - Add abstract Module class
    - Implement ModuleManager for managing modules

---
+++ src/module/module_manager.py
@@ -0,0 +1,47 @@
+import importlib.util
+import inspect
+import pathlib
+import sys
+
+from src.module.module import Module
+
+class ModuleManager:
+
+    def __init__(self):
+        self.modules = dict()
+        self.directory = None
+
+    def set_directory(self, directory: pathlib.Path):
+        self.directory = directory
+        if not directory.exists():
+            directory.mkdir()
+
+    def load(self):
+        for file in self.directory.glob("*.so"):
+            module_name = self.normalize_module_name(file)
+            module = self.load_module(module_name, file)
+            instance = self.create_module_instance(module)
+            self.modules[module_name] = instance
+
+    def normalize_module_name(self, file: pathlib.Path):
+        module_name = file.name
+        if ".cpython" in file.name:
+            module_name = file.name.split(".cpython")[0]
+        return module_name
+
+    def load_module(self, module_name, file_path):
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+        return module
+
+    def create_module_instance(self, module):
+        for key, member, in inspect.getmembers(module, inspect.isclass):
+            if issubclass(member, Module) and member is not Module:
+                return member()
+        print(f"No Module subclass found in {module}")
+        return None
+
+
+
