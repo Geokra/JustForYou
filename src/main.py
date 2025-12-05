0a90f731af1bbb0c45aa7aa3b438d14258dda9d8
1catan <jonaskipper0@gmail.com>
2025-12-05 11:36:17 +0100

    update: window added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
+import pathlib
+import sys
+from PyQt6.QtWidgets import QApplication
+from src.module.module_manager import ModuleManager
+from src.window.window import Window
+
 
 def main():
+    app = QApplication(sys.argv)
+    window = Window()
+
+    module_manager = ModuleManager()
+    module_manager.set_directory(pathlib.Path("modules"))
+    module_manager.load()
+
+    window.show()
+
+    app.exec()
     pass
 
 if __name__ == '__main__':
