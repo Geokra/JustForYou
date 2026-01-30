01e05ec3120021334699d70d046a56e5ca6a9468
1catan <jonaskipper0@gmail.com>
2026-01-30 11:34:21 +0100

    window: update

--- src/window/window.py
+++ src/window/window.py
@@ -1,6 +1,9 @@
 
 from PyQt6.QtCore import QFile, QIODeviceBase, QTextStream, Qt
-from PyQt6.QtWidgets import QHBoxLayout, QMainWindow, QStackedWidget, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QFrame, QHBoxLayout, QMainWindow, QPushButton, QStackedWidget, QTextEdit, QVBoxLayout, QWidget
+
+import history
+from settings import Settings
 
 class Window(QMainWindow):
 
@@ -16,7 +19,9 @@ class Window(QMainWindow):
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
 
-        main_layout = QHBoxLayout(self.central_widget)
+        main_layout = QVBoxLayout(self.central_widget)
+
+        top_layout = QHBoxLayout()
 
         # left section
         self.left_section = QWidget(self)
@@ -34,15 +39,48 @@ class Window(QMainWindow):
             button.clicked.connect(lambda _, w=module.widget: self.stacked_widget.setCurrentWidget(w))
             self.left_layout.addWidget(button)
 
-        main_layout.addWidget(self.left_section)
-
         for module in self.modules.values():
             self.stacked_widget.addWidget(module.widget)
 
         self.right_section_layout = QVBoxLayout(self.right_section)
         self.right_section_layout.addWidget(self.stacked_widget)
 
-        main_layout.addWidget(self.right_section)
+        top_layout.addWidget(self.left_section)
+        top_layout.addWidget(self.right_section)
+        
+        bottom_layout = QHBoxLayout()
+
+        # left bottom section
+        self.left_bottom_section = QWidget(self)
+        self.left_bottom_layout = QVBoxLayout(self.left_bottom_section)
+        self.left_bottom_section.setObjectName("left_bottom_section")
+
+        settings_button = QPushButton("Settings")
+        self.left_bottom_layout.addWidget(settings_button)
+        self.left_bottom_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter)
+        settings = Settings()
+        self.stacked_widget.addWidget(settings.widget)
+        settings_button.clicked.connect(lambda _, w=settings.widget: self.stacked_widget.setCurrentWidget(w))
+
+        self.right_bottom_section = QWidget(self)
+        self.right_bottom_section.setObjectName("right_bottom_section")
+        self.right_bottom_layout = QVBoxLayout(self.right_bottom_section)
+        self.right_bottom_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        history_text_edit = QTextEdit()
+        for line in history.history.lines:
+            history_text_edit.append(line)
+        history_text_edit.setReadOnly(True)
+        history_text_edit.setFrameShape(QFrame.Shape.NoFrame)
+
+        self.right_bottom_layout.addWidget(history_text_edit)
+
+        bottom_layout.addWidget(self.left_bottom_section)
+        bottom_layout.addWidget(self.right_bottom_section)
+
+        main_layout.addLayout(top_layout)
+        main_layout.addLayout(bottom_layout)
+        main_layout.setSpacing(0)
         main_layout.setContentsMargins(0, 0, 0, 0)
         
     def set_modules(self, modules: dict):
