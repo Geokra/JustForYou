35d98bf389d277a721719305ec4ba58d3ca0f0ad
1catan <jonaskipper0@gmail.com>
2026-02-04 19:08:37 +0100

    history: update

--- src/history.py
+++ src/history.py
@@ -1,5 +1,17 @@
 
+from PyQt6.QtWidgets import QFrame, QTextEdit
+
 class History:
-    lines = list()
+
+    def setup(self):
+        self.history_text_edit = QTextEdit()
+        self.history_text_edit.setReadOnly(True)
+        self.history_text_edit.setFrameShape(QFrame.Shape.NoFrame)
+
+    def update(self, element):
+        self.history_text_edit.append(element)
+
+    def clear(self):
+        self.history_text_edit.clear()
 
 history = History()
