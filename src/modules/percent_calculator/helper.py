53df122896539c53047c3fa92a602584f892025d
laptop <georgkrausze@gmail.com>
2026-02-05 15:30:20 +0100

    new changes

--- src/modules/percent_calculator/helper.py
+++ src/modules/percent_calculator/helper.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+mwst_brutto = 1.19
+mwst_netto = 0.19
+
+
 def calculate_percentage_davon(num: float, percentage: float):
     return percentage * num / 100
     # wie viel sind X % von Y
@@ -13,13 +17,22 @@ def calculate_percentage_weg(num:float, percentage:float):
     return num
 
 def calculate_percentage_satz(num:float, num2:float):
-    pass
+    return (num / num2) * 100
 
+def calculate_percentage_mwst_brutto(num: float):
+    return num * mwst_brutto
+
+def calculate_percentage_mwst_netto(num:float):
+    value = (num / mwst_netto)
+    num -= value
+    return num
 
 class Unit(Enum):
     davon = 1
     dazu = 2
     weg = 3
     satz = 4
+    brutto = 5
+    netto = 6
 
     # ENUM nutzen um Optionen durchzurotieren für UI Anpassung
