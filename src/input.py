e0e4fce05933402cd5cd2308b091eccff85a076f
1catan <jonaskipper0@gmail.com>
2026-03-27 12:07:33 +0100

    input: modul added

---
+++ src/input.py
@@ -0,0 +1,57 @@
+import typing
+from PyQt6 import QtGui
+from PyQt6.QtCore import Qt, pyqtSignal
+from PyQt6.QtWidgets import QWidget, QGridLayout, QLineEdit, QPushButton
+
+
+class NumPad(QWidget):
+    def __init__(self, target: QLineEdit):
+        super().__init__()
+        self.target = target
+        self.setWindowFlags(Qt.WindowType.Popup)
+
+        layout = QGridLayout(self)
+
+        nums = [
+            "7","8","9",
+            "4","5","6",
+            "1","2","3",
+            "0",",","⌫"
+        ]
+
+        for i, text in enumerate(nums):
+            btn = QPushButton(text)
+            btn.clicked.connect(lambda _, t=text: self.press(t))
+            layout.addWidget(btn, i // 3, i % 3)
+
+    def press(self, t):
+        if t == "⌫":
+            self.target.backspace()
+        else:
+            self.target.insert(t)
+
+
+class ClickableLineEdit(QLineEdit):
+    clicked = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # connect signal to slot
+        self.clicked.connect(self.open_numpad)
+    
+
+    def mousePressEvent(self, a0: typing.Optional[QtGui.QMouseEvent]) -> None:
+        self.clicked.emit()
+
+        return super().mousePressEvent(a0)
+
+    def open_numpad(self):
+
+        pad = NumPad(self)
+
+        # show under the field
+        pos = self.mapToGlobal(self.rect().bottomLeft())
+        pad.move(pos)
+        pad.show()
+
